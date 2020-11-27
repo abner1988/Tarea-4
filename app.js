032542ed8e6bb1badc25f8e7520e8d436537eea9
abner1988 <74276852+abner1988@users.noreply.github.com>
2020-11-26 18:36:23 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+var nombre = document.getElementById('nombre');
+var password = document.getElementById('password'); 
+var error = document.getElementById('error');
+error.style.color = 'red';
+
+
+function enviarFormulario(){
+console.log('Enviando formulario...');
+
+    var mensajesError = [];
+
+    if(nombre.value === null || nombre.value === ''){
+        mensajesError.push('Ingresa tu nombre');
+
+    }
+  
+    if(apellido.value === null || apellido.value === ''){
+        mensajesError.push('Ingresa tus apellidos');
+
+    }
+
+
+    if(edad.value === null || edad.value === ''){
+        mensajesError.push('Ingresa tu edad');
+
+    }
+
+    if(correo.value === null || correo.value === ''){
+        mensajesError.push('Ingresa tu correo');
+
+    }
+
+ 
+    if(password.value === null || password.value === ''){
+        mensajesError.push('Ingresa tu password');
+
+    }
+
+    error.innerHTML = mensajesError.join(',');
+
+return false;
+
+}
